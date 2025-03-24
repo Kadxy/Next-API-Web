9126606d633835861c6eb04a0881286fd6826480
Kadxy <2230318258@qq.com>
2025-03-25 03:08:47 +0800

    fix: center confirmation modal for passkey deletion to improve user experience

--- src/pages/Account/Passkey.tsx
+++ src/pages/Account/Passkey.tsx
@@ -72,6 +72,7 @@ const PasskeyManager: FC = () => {
         Modal.confirm({
             title: '确定要删除这个通行密钥吗?',
             content: '删除后将无法使用此通行密钥登录',
+            centered: true,
             onOk: async () => {
                 try {
                     await handleResponse(api.passkeyAuthentication.passkeyControllerDeletePasskey({ id }), {
